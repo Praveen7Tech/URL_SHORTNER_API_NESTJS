40ae48eba4c5dcaddd35b7828a7fab84874c6dd6
Pr4veenn <praveen.tech.js@gmail.com>
2026-03-20 22:08:36 +0530

    cookie issue updated

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.set('trust proxy', 1);
 
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
@@ -16,8 +19,8 @@ async function bootstrap() {
   
   app.enableCors({
     origin:[
-      process.env.CLIENT_URL_DEV,
-      process.env.CLIENT_URL_PRODUCTION
+      process.env.CLIENT_URL_DEV!,
+      process.env.CLIENT_URL_PRODUCTION!
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
